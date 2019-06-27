5fa09119b8fbe71044cb65477951829e78628bb8
Vishakha Vidhani <vhvidhani12@gmail.com>
2019-06-27 12:44:09 +0530

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function index(){
+        return view('index');
+    }
+
+    public function about(){
+        return view('about');
+    }
+
+    public function services(){
+        return view('services');
+    }
+}
